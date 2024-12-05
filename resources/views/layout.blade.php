f73842250f4b006ed67aa82bac6a6b8d0a817be9
muhammad medhat <muhammad.medhat@gmail.com>
2024-12-05 21:46:51 +0200

    adding html template

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite('resources/css/app.css')
     <title>Lara jobs</title>
 </head>
-<body class='bg-grey-100'>
+<body class='bg-gray-100'>
     {{-- @include('partials.navbar') --}}
     <x-header />
     <main class="content mx-auto bg-grey-400">
